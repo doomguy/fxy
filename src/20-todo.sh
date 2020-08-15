7205783e98ee43ba7ef7ca0cbd16f0f2f98318fa
doomguy <doomguy>
2020-08-15 20:57:14 +0200

    todo

--- src/20-todo.sh
+++ src/20-todo.sh
@@ -3,3 +3,12 @@ if [ "$#" -eq 1 ] && [ "$1" == "2do" ]; then
   grep -Ein "^##|^# 2DO|^# -" "$0" | sed 's,\\t.*,,'
   exit
 fi
+
+# gobuster dir -u http://10.10.10.191/ -w /usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt -x php
+# msf multi handler
+# msf venom most common
+# smbclient
+# smbmap
+# enum4linux
+# bash one liners
+# user agent
--- src/99-footer.sh
+++ src/99-footer.sh
@@ -1,11 +1,2 @@
-# gobuster dir -u http://10.10.10.191/ -w /usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt -x php
-# msf multi handler
-# msf venom most common
-# smbclient
-# smbmap
-# enum4linux
-# bash one liners
-# user agent
-
 # Nothing found? Invalid command?
 showHelp; exit
