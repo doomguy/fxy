41058016c6d9b18d0939a031f74ad92bea9bbbe2
doomguy <doomguy>
2020-08-19 22:42:42 +0200

    added fxy update cmd

---
+++ src/20-update.sh
@@ -0,0 +1,19 @@
+## update^: Update fxy
+if [ "$1" == "update" ]; then
+  if [ $(which git) ]; then
+    FXYPATH=$(sed 's,fxy$,,' <<< $0)
+    cd "$FXYPATH" || exit
+    if [ $(git config remote.origin.url) == "git@github.com:doomguy/fxy.git" ]; then
+      git pull
+      exit
+    fi
+  else
+    CMD="wget"
+    INSTCMD="sudo apt install wget"
+    checkCmd
+
+    wget https://raw.githubusercontent.com/doomguy/fxy/master/build/fxy -O "$0"
+    chmod +x "$0"
+    exit
+  fi
+fi
