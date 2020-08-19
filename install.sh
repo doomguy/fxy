fb343b38bcb8660ce58ad71c201c92817cbc7384
doomguy <doomguy>
2020-08-19 23:21:36 +0200

    install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
+#!/usr/bin/env bash
 FXYPATH="/usr/local/bin/fxy"
-echo wget 'https://raw.githubusercontent.com/doomguy/fxy/master/build/fxy' -O "$FXYPATH"
+curl 'https://raw.githubusercontent.com/doomguy/fxy/master/build/fxy' -o "$FXYPATH"
 chmod +x "$FXYPATH"
 echo "[*] fxy has been installed to '$FXYPATH'"
