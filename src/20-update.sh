1cda93e7f47dd7500f527e465dc5729217e97bf5
doomguy <doomguy>
2020-08-19 22:48:51 +0200

    trblsh00ting

--- src/20-update.sh
+++ src/20-update.sh
@@ -4,7 +4,7 @@ if [ "$1" == "update" ]; then
     FXYPATH=$(sed 's,fxy$,,' <<< "$0")
     cd "$FXYPATH" || exit
     if [ "$(git config remote.origin.url)" == "git@github.com:doomguy/fxy.git" ]; then
-      git pull
+      echo git pull
       exit
     fi
   else
@@ -12,7 +12,7 @@ if [ "$1" == "update" ]; then
     INSTCMD="sudo apt install wget"
     checkCmd
 
-    wget 'https://raw.githubusercontent.com/doomguy/fxy/master/build/fxy' -O "$0"
+    echo wget 'https://raw.githubusercontent.com/doomguy/fxy/master/build/fxy' -O "$0"
     chmod +x "$0"
     exit
   fi
