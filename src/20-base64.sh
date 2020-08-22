d9c74af361a6634295e576fd30f7ee201ed39b3c
doomguy <doomguy>
2020-08-22 11:05:03 +0200

    base64: disabled wrapping for encoding

--- src/20-base64.sh
+++ src/20-base64.sh
@@ -2,7 +2,7 @@
 if [[ "$1" =~ ^b(ase)?64(e|d)?$ ]] && [ "$#" -ge 2 ]; then
   CMD="base64"; checkCmd
   case "$1" in
-    "b64"|"b64e"|"base64"|"base64e")  CMD="base64" ;;
+    "b64"|"b64e"|"base64"|"base64e")  CMD="base64 -w0" ;;
     "b64d"|"base64d")                 CMD="base64 -d" ;;
     *)                                echo "${warn} Error parsing type of operation!"; exit ;;
   esac
@@ -10,7 +10,7 @@ if [[ "$1" =~ ^b(ase)?64(e|d)?$ ]] && [ "$#" -ge 2 ]; then
   # in encoding mode:
     # if more than 2 args, use args >=2 as input
   INPUT="$2"
-  if [ "$CMD" == "base64" ] && [ "$#" -ge 3 ]; then
+  if [ "$CMD" == "base64 -w0" ] && [ "$#" -ge 3 ]; then
     INPUT="$(echo "$@" | cut -d' ' -f2-)"
   fi
 
