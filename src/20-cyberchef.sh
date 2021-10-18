a8c07c19e24d8c49375f47802a327c86077d9eb8
Doomguy <1833568+doomguy@users.noreply.github.com>
2021-10-18 23:55:22 +0200

    fix cyberchef magic cmd

--- src/20-cyberchef.sh
+++ src/20-cyberchef.sh
@@ -5,7 +5,7 @@ if [[ "$1" =~ ^(cyber)?chef$ ]]; then
 
   if [ "$#" -eq 3 ] && [ "$2" == "magic" ]; then
     INPUT=$(echo -n "$3" | base64 | sed 's,=,%3D,g')
-    CMD='xdg-open "https://gchq.github.io/CyberChef/#recipe=Magic(3,false,false,'')&input='${INPUT}"
+    CMD="xdg-open 'https://gchq.github.io/CyberChef/#recipe=Magic(3,false,false,%27%27)&input=${INPUT}'"
   fi
 
   echo "${bldwht}> $CMD${txtrst}"
