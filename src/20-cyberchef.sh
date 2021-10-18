f95dfb3d95295d4bf1373778e8af9a1175664693
Doomguy <1833568+doomguy@users.noreply.github.com>
2021-10-18 23:42:04 +0200

    chef magic improvements

--- src/20-cyberchef.sh
+++ src/20-cyberchef.sh
@@ -1,13 +1,11 @@
 ## (cyber)chef [magic]^: Open CyberChef in your browser
 if [[ "$1" =~ ^(cyber)?chef$ ]]; then
-  CMD="firefox"; checkCmd
-  CMD="firefox https://gchq.github.io/CyberChef/"
+  CMD="xdg-open"; checkCmd
+  CMD="xdg-open https://gchq.github.io/CyberChef/"
 
   if [ "$#" -eq 3 ] && [ "$2" == "magic" ]; then
-    INPUT=$(echo -n "$3" | base64 | sed 's,=,,g')
-    echo "Run this in your browser (I can't do it for you!):"
-    echo "  https://gchq.github.io/CyberChef/#recipe=Magic(3,false,false,'')&input=$INPUT"
-    exit
+    INPUT=$(echo -n "$3" | base64 | sed 's,=,%3D,g')
+    CMD="xdg-open https://gchq.github.io/CyberChef/#recipe=Magic(3,false,false,'')&input=${INPUT}"
   fi
 
   echo "${bldwht}> $CMD${txtrst}"
